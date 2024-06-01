725a1c3858b53ba00ab8dc55dd376bcd55c8d200
Aurajakti <146406924+Aurajakti@users.noreply.github.com>
2024-06-01 20:20:32 +0700

    Training day-1.py
    learning based of python

---
+++ day-1.py
@@ -0,0 +1,21 @@
+#komentar
+#ijij
+print("hello python")
+print(13)
+
+nama = "kevin" #kata or kalimat wajib vakai "" (string)
+hobi = "ngoding" #same
+umur = 15 #tipe data or iNTeger
+perempuan = False #true or fales or boolean
+
+
+#6 angka pertama = 1
+nama_lengkap = "kevin mark heller" #snake_case
+nama = "mark" 
+koma = ","
+print(nama + str(umur))
+
+print(nama, umur)
+print("nama saya adalah", nama + ", umur saya", umur )
+
+print(hobi)
